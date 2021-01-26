085277f97c9a464789cf6d3e58066d4d6813f23f
Skgland <bb-github@t-online.de>
2021-01-26 10:55:10 +0100

    further minimized

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,34 @@
-use std::ops::{BitXor, Shl};
+use std::ops::BitXor;
 
-pub trait Protocol {
-    type PacketIndex: Into<u64> + Into<BigUint>;
+pub struct S;
+
+pub trait P {
+    type I: Into<u64> + Into<S>;
 }
 
-fn from_nothing<T>() -> T {
-    todo!()
+pub fn decrypt_portion<T: P>(index: T::I) {
+    let iv = S ^ index.into();
+    &iv.to_bytes_be();
 }
 
-pub fn decrypt_portion<P: Protocol>(index: P::PacketIndex) {
-    let iv: BigUint = from_nothing();
-    let len: usize = from_nothing();
-    let iv = iv ^ (index.into() << 16);
-    let iv = iv ^ (BigUint::from(1_u8) << (len * 8));
-    let _iv: &[u8] = &iv.to_bytes_be()[1..len + 1];
-}
-
-pub struct BigUint;
-
-impl BigUint {
-    fn to_bytes_be(&self) -> Vec<u8> {
-        todo!()
-    }
-}
-
-impl From<u8> for BigUint {
-    fn from(_: u8) -> Self {
+impl S {
+    fn to_bytes_be(&self) -> &[u8] {
         unimplemented!()
     }
 }
 
-impl Shl<usize> for BigUint {
-    type Output = BigUint;
-
-    fn shl(self, _rhs: usize) -> Self::Output {
-        unimplemented!()
-    }
-}
-
-impl BitXor for BigUint {
-    type Output = BigUint;
+impl BitXor for S {
+    type Output = S;
 
     fn bitxor(self, _rhs: Self) -> Self::Output {
         unimplemented!()
     }
 }
 
-impl<'a> BitXor<&'a BigUint> for BigUint {
-    type Output = BigUint;
+impl<'a> BitXor<&'a S> for S {
+    type Output = S;
 
-    fn bitxor(self, _rhs: &'a BigUint) -> Self::Output {
+    fn bitxor(self, _rhs: &'a S) -> Self::Output {
         unimplemented!()
     }
 }
